ef2c52d63e50ec807cc407513f5bbca6cd01ef84
Sanduni Navodya <128354320+SanduniNavodya@users.noreply.github.com>
2024-06-27 14:02:50 +0530

    Completed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Weather from './components/Weather'
+import React from 'react';
+import Weather from './components/Weather';
 
 const App = () => {
   return (
     <div className='app'>
       <Weather/>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
